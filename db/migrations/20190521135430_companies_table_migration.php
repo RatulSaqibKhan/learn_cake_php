9ab55868db16cccdcafd5a7fcf19852249ede471
ratulsaqibkhan <ratulkhan.jhenidah@gmail.com>
2019-05-21 22:59:05 +0600

    Learn Migration using phinx

---
+++ db/migrations/20190521135430_companies_table_migration.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CompaniesTableMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $companies = $this->table('companies');
+        $companies->addColumn('name', 'string')
+            ->addColumn('address', 'text', ['null' => true])
+            ->addColumn('created_at', 'datetime', ['null' => true])
+            ->addColumn('updated_at', 'datetime', ['null' => true])
+            ->save();
+    }
+
+    public function down()
+    {
+        $this->dropTable('companies');
+    }
+}
---
+++ db/migrations/20190521164631_users_table_migration.php
@@ -0,0 +1,47 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UsersTableMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $users = $this->table('users');
+        $users->addColumn('username', 'string', ['limit' => 30])
+            ->addColumn('password', 'string', ['limit' => 40])
+            ->addColumn('email', 'string', ['limit' => 100])
+            ->addColumn('first_name', 'string', ['limit' => 30])
+            ->addColumn('last_name', 'string', ['limit' => 30])
+            ->addColumn('company_id', 'integer')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime', ['null' => true])
+            ->addIndex(['username', 'email'], ['unique' => true])
+            ->addForeignKey('company_id', 'companies', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
+            ->save();
+    }
+}
